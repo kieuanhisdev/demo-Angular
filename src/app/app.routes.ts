381bbb131317308dd5a578e9a4c92342ddb54d5a
Kieu Anh <kieuanh.dev@gmail.com>
2024-05-27 20:30:43 +0700

    2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
-    {path:'', component:LoginComponent}
+    { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect tới /login
+    { path: 'login', component: LoginComponent },
+    {path: 'register', component: RegisterComponent}
 ];
